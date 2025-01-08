036be2d0986779dc7abc75aadb6cf7170c6eab99
Aniruddhaoffice <144651519+Aniruddhaoffice@users.noreply.github.com>
2025-01-08 09:41:36 +0000

    variable and datatype done

---
+++ Basic/datatype.js
@@ -0,0 +1,4 @@
+"use strict";// treate all js code as newer version 
+
+//alert(3+3) we are using node js 
+
---
+++ Basic/variable.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "ani@gmail.com"
+var accountPass = "12345"
+accountCity = "west bengal"
+let accountState
+
+
+//accountId = 2 //not allowed
+
+
+//console.log(accountId);
+
+console.table([accountId,accountEmail,accountPass,accountCity,accountState])
+/*
+prefer not to use "var" ,beacause of issue of block scope
+*/
