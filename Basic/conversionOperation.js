4fc17c106b4dc0844e26b5208558d0ce24c4031f
Aniruddha Saha <144651519+Aniruddhaoffice@users.noreply.github.com>
2025-02-20 20:07:08 +0000

    upto comparition done

--- Basic/conversionOperation.js
+++ Basic/conversionOperation.js
@@ -3,3 +3,13 @@
 // let value = Number(score)
 // console.log(typeof value)
 //1 = true ,0 = false
+
+
+// let a = 3
+// let b = 4
+// let c = "aniruddha"
+// let d = a+b+c
+// console.log(d);
+
+let gameCounter = 100
+console.log(++gameCounter);
