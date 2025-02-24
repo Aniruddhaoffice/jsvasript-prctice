29e144d208a128c73efcdcf2b4d7ee1d29db0ff4
Aniruddha Saha <144651519+Aniruddhaoffice@users.noreply.github.com>
2025-02-24 19:29:14 +0000

    24-02-2025

--- Basic/comparition.js
+++ Basic/comparition.js
@@ -4,3 +4,4 @@
 // console.log(null>=1);
 // console.log(undefined==1);
 // console.log("2"===1);
+// console.log("1"==1);
---
+++ Basic/datatype-summary.js
@@ -0,0 +1,18 @@
+//primitive 
+//-> string, number, boolean, null, undifiend, symbol, Bigint.
+
+//reference type / non-primitive
+//-> Array, objects, Functions
+
+//javascript dynamically language
+//there is no float or decimals number in javascript ,all number is a number.
+
+//symbol
+// const id =Symbol('123')
+// const anotherid =Symbol('123')
+// console.log(id === anotherid)
+//->output -> false 
+// const binum =82746477n
+// console.log(typeof binum)
+
+const array = ["saktiman",""]
