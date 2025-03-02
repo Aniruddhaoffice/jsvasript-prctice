8fc723cd52f7d9db20e1bec2983507a3b7ff8610
Aniruddha Saha <144651519+Aniruddhaoffice@users.noreply.github.com>
2025-03-02 09:10:57 +0000

    02-03-2025

---
+++ Basic/strings.js
@@ -0,0 +1,27 @@
+// const name = "asit"
+// const age = 29
+// console.log(name+age);
+// console.log(`hello my name is ${name} and my name is ${age}`);
+
+
+const gameName = new String('Aniruddha')  //goto browser and inspect and find the prototype 
+
+// console.log(gameName.length);
+// console.log(gameName[0]);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(3)); //find position of object 
+// console.log(gameName.indexOf('u')); //find position of object 
+// console.log(gameName.substring(0,4)); //find position of object / negative value will be ignore
+// console.log(gameName.newString);
+
+
+// const anotherString =" babai "
+// console.log(anotherString);
+// console.log(anotherString.trim());
+
+const url = "https://ani.com/aniruddha%50saha"
+
+console.log(url);
+console.log(url.replace('%50','_'));
+
+console.log(url.includes('aniruddha')) //search any keywords in the value
