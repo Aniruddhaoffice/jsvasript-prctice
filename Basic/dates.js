bdcd6388dd0defd53820f9b3ceca9559ba4b6208
Aniruddha Saha <144651519+Aniruddhaoffice@users.noreply.github.com>
2025-03-19 19:08:23 +0000

    19-03-2025

---
+++ Basic/dates.js
@@ -0,0 +1,21 @@
+let myDate = new Date()
+
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+console.log(myDate.getMonth()); 
+//in js date alawas count from 0 , as january = 0 ,February = 1 . 
+
+
+let myCreDate = new Date(2025,0,19)
+let myWonCreDate = new Date("01-14-2025")
+
+// console.log(myCreDate.toLocaleString());
+// console.log(myWonCreDate.toLocaleString());
+
+let myTimestamp = Date.now()
+// console.log(myTimestamp);
+// console.log(myWonCreDate.getTime());
+// console.log(Date.now()/1000);
+// console.log(Math.floor(Date.now()/1000));
+
---
+++ Basic/part2/array.js
@@ -0,0 +1,7 @@
+// console.log('hello..starting array');
+
+const myArr = [1,2,3,4,5]
+
+console.log(myArr);
+console.log(myArr.push(6));
+console.log(myArr);
