67285216ed40a01ed47f8bcbe7eae45166d18b06
Aniruddha Saha <144651519+Aniruddhaoffice@users.noreply.github.com>
2025-03-27 20:28:17 +0000

    28-03-2025 1:58 AM

---
+++ Basic/part3/scopes.js
@@ -0,0 +1,20 @@
+// console.log("learnig scopes in js ->");
+
+let a = 300
+
+if(true){
+    let a = 20
+
+    const b = 30
+    
+    var c = 40 
+    console.log("inner :",a);
+    
+
+}
+
+
+
+console.log(a);
+// console.log(b);
+// console.log(c);
